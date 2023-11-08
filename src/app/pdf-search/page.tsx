299649b299658ee0b395afd195796401fd89234e
Terry Zhang <terrychinaz@gmail.com>
2023-11-08 12:56:25 +0800

    feat: update ui

--- src/app/pdf-search/page.tsx
+++ src/app/pdf-search/page.tsx
@@ -9,7 +9,7 @@ export default function Component() {
   return (
     <section className="space-y-6 w-4/5 m-auto">
       <div className="grid w-full max-w-sm items-center p-4 md:p-6">
-        <Label htmlFor="pdf">Upload PDF</Label>
+        <Label className="text-md py-4">Upload PDF/Files/Image</Label>
         <Input accept=".pdf" id="pdf" type="file" />
       </div>
       {/* <div className="w-4/5"> */}
